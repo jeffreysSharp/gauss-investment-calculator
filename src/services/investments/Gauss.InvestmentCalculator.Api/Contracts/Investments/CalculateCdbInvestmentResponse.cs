b6a01c6f0151d9fcb8f968faa2624f599438da40
Jeferson Almeida <jeffreys.sharp@outlook.com>
2026-04-28 21:41:21 -0300

    feat(api): add cdb simulation endpoint

---
+++ src/services/investments/Gauss.InvestmentCalculator.Api/Contracts/Investments/CalculateCdbInvestmentResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Gauss.InvestmentCalculator.Api.Contracts.Investments;
+
+public sealed record CalculateCdbInvestmentResponse(
+    decimal InitialAmount,
+    int TermInMonths,
+    decimal GrossAmount,
+    decimal ProfitAmount,
+    decimal IncomeTaxRate,
+    decimal IncomeTaxAmount,
+    decimal NetAmount);
